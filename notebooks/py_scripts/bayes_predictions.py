d62bb99f22e5f8c1f884ed0678509f698f1c1dd4
David Reguera <birrick@gmail.com>
2022-08-06 11:32:45 +0200

    Pass the linter

--- notebooks/py_scripts/bayes_predictions.py
+++ notebooks/py_scripts/bayes_predictions.py
@@ -21,14 +21,14 @@ class BayesPredictions:
         self.risk = risk
 
         # Compute price and off ranges
-        self.price_range = np.linspace(10_000, 80_000, 1401)  # min showcase 18.3k, max 71.6k
+        self.price_range = np.linspace(
+            10_000, 80_000, 1401
+        )  # min showcase 18.3k, max 71.6k
         self.off_ranges = self.get_off_ranges()
 
-        self.outcomes = pd.DataFrame({
-            "p1_wins": [0.],
-            "p2_wins": [0.],
-            "no_one_wins": [0.]
-        })
+        self.outcomes = pd.DataFrame(
+            {"p1_wins": [0.0], "p2_wins": [0.0], "no_one_wins": [0.0]}
+        )
 
     def fix_data(self, player):
         """Determine the values for the current estimation."""
@@ -58,23 +58,25 @@ class BayesPredictions:
         """Read the showcase price data from the csv."""
         df = pd.read_csv(filename, index_col=0, skiprows=[1])
         df = df.dropna().transpose()
-        df = df.rename(columns={
-            'Showcase 1': 'showcase_1',
-            'Showcase 2': 'showcase_2',
-            'Bid 1': 'bid_1',
-            'Bid 2': 'bid_2',
-            'Difference 1': 'diff_1',
-            'Difference 2': 'diff_2',
-        })
+        df = df.rename(
+            columns={
+                "Showcase 1": "showcase_1",
+                "Showcase 2": "showcase_2",
+                "Bid 1": "bid_1",
+                "Bid 2": "bid_2",
+                "Difference 1": "diff_1",
+                "Difference 2": "diff_2",
+            }
+        )
         # it turns out that diffs are swapped
-        df.loc[:, 'diff_1'] = -df.diff_1
-        df.loc[:, 'diff_2'] = -df.diff_2
+        df.loc[:, "diff_1"] = -df.diff_1
+        df.loc[:, "diff_2"] = -df.diff_2
         return df
 
     def create_base_df(self):
         """Create a dataframe out of the outcomes of historical data."""
-        df2011 = self.read_historical_data('data/showcases.2011.csv')
-        df2012 = self.read_historical_data('data/showcases.2012.csv')
+        df2011 = self.read_historical_data("data/showcases.2011.csv")
+        df2012 = self.read_historical_data("data/showcases.2012.csv")
         return pd.concat([df2011, df2012], ignore_index=True)
 
     def get_main_df(self):
@@ -88,7 +90,7 @@ class BayesPredictions:
         base_df["estimated_diff1"] = base_df.diff_1
         base_df["estimated_bid2"] = base_df.bid_1
         base_df["estimated_diff2"] = base_df.diff_2
-        base_df.iloc[self.initial_history:, 6:] = np.nan
+        base_df.iloc[self.initial_history :, 6:] = np.nan
         return base_df
 
     def get_off_ranges(self):
@@ -110,7 +112,9 @@ class BayesPredictions:
 
         """
         prior = gaussian_kde(self.showcase_history).pdf(self.price_range)
-        likelihood = norm(self.current_bid, self.error_history.std()).pdf(self.price_range)
+        likelihood = norm(self.current_bid, self.error_history.std()).pdf(
+            self.price_range
+        )
         posterior = prior * likelihood
         return posterior / posterior.sum()
 
@@ -156,7 +160,9 @@ class BayesPredictions:
         """
         self.fix_data(player)
         bt = self.bayes_estimator()
-        chances_ranges = np.apply_along_axis(self.get_chances_of_winning, 1, self.off_ranges)
+        chances_ranges = np.apply_along_axis(
+            self.get_chances_of_winning, 1, self.off_ranges
+        )
 
         winning_bid_distribution = (chances_ranges.T * bt).sum(axis=1)
         winning_bid_distribution /= winning_bid_distribution.sum()
@@ -167,33 +173,37 @@ class BayesPredictions:
         """Save the estimation onto the dataframe."""
         estimate = self.estimate_winning_bid(player=1)
         estimated_row = self.df[self.df.estimated_bid1.isna()].index[0]
-        real_price = self.df.loc[estimated_row, 'showcase_1']
+        real_price = self.df.loc[estimated_row, "showcase_1"]
 
-        self.df.loc[estimated_row, 'estimated_bid1'] = estimate
-        self.df.loc[estimated_row, 'estimated_diff1'] = estimate - real_price
+        self.df.loc[estimated_row, "estimated_bid1"] = estimate
+        self.df.loc[estimated_row, "estimated_diff1"] = estimate - real_price
 
         # and for player_score 2
         estimate = self.estimate_winning_bid(player=2)
         estimated_row = self.df[self.df.estimated_bid2.isna()].index[0]
-        real_price = self.df.loc[estimated_row, 'showcase_2']
+        real_price = self.df.loc[estimated_row, "showcase_2"]
 
-        self.df.loc[estimated_row, 'estimated_bid2'] = estimate
-        self.df.loc[estimated_row, 'estimated_diff2'] = estimate - real_price
+        self.df.loc[estimated_row, "estimated_bid2"] = estimate
+        self.df.loc[estimated_row, "estimated_diff2"] = estimate - real_price
 
     def compute_outcome(self):
         p1_gt_p2 = self.df.estimated_diff1 > self.df.estimated_diff2
         p1_overbids = self.df.estimated_diff1 > 0  # 24%
         p2_overbids = self.df.estimated_diff2 > 0  # 28%
-        both_underbid = (~p1_overbids & ~p2_overbids)  # 53%
-        both_overbid = (p1_overbids & p2_overbids)  # 6.7%
+        both_underbid = ~p1_overbids & ~p2_overbids  # 53%
+        both_overbid = p1_overbids & p2_overbids  # 6.7%
 
-        p1_underbids_by_less = (p1_gt_p2 & both_underbid)  # 25.6%
-        p2_underbids_by_less = (~p1_gt_p2 & both_underbid)  # 27.8%
+        p1_underbids_by_less = p1_gt_p2 & both_underbid  # 25.6%
+        p2_underbids_by_less = ~p1_gt_p2 & both_underbid  # 27.8%
 
         # Players win when they don't overbid AND either their opponent overbids OR they underbid
         # by less when their opponent does not overbid
-        p1_wins = ((p1_underbids_by_less | p2_overbids) & ~p1_overbids).mean()  # 54% * 76% = 41%
-        p2_wins = ((p2_underbids_by_less | p1_overbids) & ~p2_overbids).mean()  # 52% * 72% = 37%
+        p1_wins = (
+            (p1_underbids_by_less | p2_overbids) & ~p1_overbids
+        ).mean()  # 54% * 76% = 41%
+        p2_wins = (
+            (p2_underbids_by_less | p1_overbids) & ~p2_overbids
+        ).mean()  # 52% * 72% = 37%
         no_winner = both_overbid.mean()
 
         results = (p1_wins, p2_wins, no_winner)
@@ -235,7 +245,7 @@ class BayesPredictions:
     @staticmethod
     def print_outcome(results):
         if np.random.uniform() > 9 / 10:  # print some results once in a while.
-            print(50 * '*')
+            print(50 * "*")
             p1_wins, p2_wins, no_winner = results
             print(f"player_score 1 wins: {100 * p1_wins.round(3)}% of the time")
             print(f"player_score 2 wins: {100 * p2_wins.round(3)}% of the time")
@@ -274,17 +284,21 @@ class MakeUpP1(BayesPredictions):
         (worse if it's higher than one).
         """
         df = super().create_base_df()
-        df.loc[:, 'bid_1'] = np.random.normal(df.showcase_1 * self.worsen_factor, df.diff_1.std())
-        df.loc[:, 'diff_1'] = df.bid_1 - df.showcase_1
+        df.loc[:, "bid_1"] = np.random.normal(
+            df.showcase_1 * self.worsen_factor, df.diff_1.std()
+        )
+        df.loc[:, "diff_1"] = df.bid_1 - df.showcase_1
         return df
 
     def save_data_to_file(self):
         f = str(self.worsen_factor).replace(".", "_")
         self.df.to_csv(f"09-outcomes/showcases-{self.initial_history}-{f}-worse.csv")
-        self.outcomes.to_csv(f"09-outcomes/outcomes-{self.initial_history}-{f}-worse.csv")
+        self.outcomes.to_csv(
+            f"09-outcomes/outcomes-{self.initial_history}-{f}-worse.csv"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     initial_history = 4
 
     # Comment one of the following, so it won't take very long to compute:
