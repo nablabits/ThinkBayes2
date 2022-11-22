dce7cf0bbcceff9542ddeebed86c076f2b934bbf
David Reguera <birrick@gmail.com>
2022-11-22 06:55:49 +0100

    Add the multinomial estimator file

--- notebooks/py_scripts/weibull_predictor.py
+++ notebooks/py_scripts/weibull_predictor.py
@@ -4,13 +4,12 @@ from scipy.stats import weibull_min
 
 
 class WeibullEstimator:
-
     def __init__(
         self,
         lifetime: np.ndarray = None,
         arrival_date: np.ndarray = None,
         lbd_range: np.ndarray = None,
-        k_range: np.ndarray = None
+        k_range: np.ndarray = None,
     ):
         self.lifetime = lifetime
         self.arrival_date = arrival_date
@@ -20,7 +19,7 @@ class WeibullEstimator:
     def auto_generate(self):
         sample = 10
         hs_size = 101
-        dist = weibull_min(.8, scale=3)
+        dist = weibull_min(0.8, scale=3)
         self.lifetime = dist.rvs(sample)
         self.arrival_date = np.random.uniform(8, size=sample)
         self.lbd_range = np.linspace(0.1, 10.1, hs_size)
@@ -34,8 +33,6 @@ class WeibullEstimator:
 
     def likes_with_sf(self):
         pass
-
-
 
     def run(self):
         if self.lifetime is None:
